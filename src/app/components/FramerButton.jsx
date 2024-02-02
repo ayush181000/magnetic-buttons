71fb27e96fed6504296fcdb5d8ffafe65da7d3c3
Ayush Garg <ayush181000@gmail.com>
2024-02-02 16:48:07 +0530

    framer finish

--- src/app/components/FramerButton.jsx
+++ src/app/components/FramerButton.jsx
@@ -1,5 +1,38 @@
+'use client';
+import { useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+
 const FramerButton = ({ children }) => {
-  return <div>{children}</div>;
+  const ref = useRef(null);
+  const [positon, setPosition] = useState({ x: 0, y: 0 });
+
+  const mouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = ref.current.getBoundingClientRect();
+
+    const x = clientX - (left + width / 2);
+    const y = clientY - (top + height / 2);
+
+    setPosition({ x, y });
+  };
+
+  const mouseLeave = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = positon;
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      animate={{ x, y }}
+      transition={{ type: 'spring', stiffness: 150, damping: 15, mass: 0.1 }}
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default FramerButton;
