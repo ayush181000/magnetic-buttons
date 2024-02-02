3becaffc1bd5e0ef834dd405f7885116e135f921
Ayush Garg <ayush181000@gmail.com>
2024-02-02 16:21:08 +0530

    boiler

---
+++ src/app/components/FramerButton.jsx
@@ -0,0 +1,5 @@
+const FramerButton = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default FramerButton;
---
+++ src/app/components/GsapMagneticButton.jsx
@@ -0,0 +1,5 @@
+const GsapMagneticButton = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default GsapMagneticButton;
